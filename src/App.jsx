fecc8dd65cdebb73844c78d4fed50103df8fa483
jesgodoy <jesicagodoy7@gmail.com>
2024-04-16 18:38:10 -0300

    agrego Item List Container

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import './style.css'
 import NavBar from './components/navbar/NavBar'
+import ItemListContainer from './components/item-list-container/ItemListContainer.jsx'
 
 function App() {
   return (
-    <NavBar />
+    <div>
+      <NavBar />
+      <ItemListContainer greeting={'Bienvenidos a Gamer Squad'} />
+    </div>
+    
   )
 }
 
