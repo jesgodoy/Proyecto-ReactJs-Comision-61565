e4098ca96e72784c57dcf1743a5cf2ce9c1c7012
jesgodoy <jesicagodoy7@gmail.com>
2024-05-08 17:06:37 -0300

    creacion item Count

---
+++ src/components/item-count/ItemCount.jsx
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+
+
+const ItemCount =({stock, initial, onAdd})=>{
+    const [quantity, setQuantity]  = useState (initial)
+    
+    
+    const handleAdd = () => quantity < stock ? setQuantity(quantity+1): null
+    
+    const handleSubstract = ()=>quantity > 1 ? setQuantity(quantity - 1) : null;
+
+    return(
+        <div className='container'>
+            <div className='container text-center d-flex'>
+                <button type="button" className="btn btn-secondary" onClick={handleSubstract}>-</button>
+                <h4 className='p-1 mx-2'>{quantity}</h4>
+                <button type="button" className="btn btn-secondary"  onClick={handleAdd}>+</button>
+            </div>
+            <div>
+                <button className='rounded mt-3 p-3 button-count' onClick={()=>  onAdd(quantity)} disabled={!stock}>
+                    {stock ? 'agregar al carrito' : 'No hay stock disponible'}
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default ItemCount
